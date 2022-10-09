9e2fc52b5626e6c14a055a8e4e4270f4c1f865ac
Yan Borges <yandsb16@gmail.com>
2022-10-09 17:19:53 -0400

    convert to ts

--- src/components/RepositoryList.tsx
+++ src/components/RepositoryList.tsx
@@ -3,8 +3,14 @@ import { RepositoryItem } from "./RepositoryItem";
 import '../styles/repositories.scss'
 import { useEffect, useState } from "react";
 
+interface Repository {
+  name: string;
+  description: string;
+  html_url: string;
+}
+
 export function RepositoryList(){
-  const [repositories, setRepositories] = useState([])
+  const [repositories, setRepositories] = useState<Repository[]>([])
 
   useEffect(() => {
     fetch('https://api.github.com/orgs/rocketseat/repos', { method: 'GET' })
@@ -19,7 +25,7 @@ export function RepositoryList(){
       <h1>Lista de repositórios</h1>
       <ul>
         {repositories.map(repository => {
-          return <RepositoryItem key={repository.id} repository={repository} />
+          return <RepositoryItem key={repository.name} repository={repository} />
         })}
       </ul>
     </section>
