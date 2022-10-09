0c1296dd1fbb5e02a9f5766808278c535891f7d2
Yan Borges <yandsb16@gmail.com>
2022-10-09 15:43:43 -0400

    get repos and set in sate

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -1,30 +1,27 @@
 import { RepositoryItem } from "./RepositoryItem";
 
 import '../styles/repositories.scss'
-
-const repository = {
-  name: 'unform',
-  description:'Forms in React',
-  link: 'https://github.com/unform/unform'
-}
+import { useEffect, useState } from "react";
 
 export function RepositoryList(){
+  const [repositories, setRepositories] = useState([])
+
+  useEffect(() => {
+    fetch('https://api.github.com/orgs/rocketseat/repos', { method: 'GET' })
+    .then(response => response.json())
+    .then(data => {
+      const dataFormatted = data.map(repo => {
+        return { ...repo, link: repo.html_url}
+      })
+      setRepositories(dataFormatted)
+    })
+  }, [])
+
   return (
     <section className="repository-list">
       <h1>Lista de repositórios</h1>
       <ul>
-        <RepositoryItem
-          repository={repository}
-        />
-        <RepositoryItem
-          repository={repository}
-        />
-        <RepositoryItem
-          repository={repository}
-        />
-        <RepositoryItem
-          repository={repository}
-        />
+        {repositories.map(repository => (<RepositoryItem key={repository.id} repository={repository} />))}
       </ul>
     </section>
   )
