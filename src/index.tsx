1d424ebd9cbc3aae0e0dcf20ef589726cc1d44c3
Yan Borges <yandsb16@gmail.com>
2022-10-09 17:11:03 -0400

    config typescript

---
+++ src/index.tsx
@@ -0,0 +1,8 @@
+import ReactDOM from 'react-dom/client';
+import { App } from './App';
+
+const container = document.getElementById('root') as HTMLElement;
+
+const root = ReactDOM.createRoot(container)
+
+root.render(<App />);
